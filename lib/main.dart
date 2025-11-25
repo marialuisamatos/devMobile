6655bdb8b770cd454b8c3d3bd03394d3bd43d661
Andre Franca <andre.dfranca@outlook.com>
2025-11-24 23:57:00 -0300

    Adicionando configuração do Firebase e integrando autenticação ao projeto Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutterfirebaselogin/login.dart';
 import 'package:flutterfirebaselogin/interna.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MaterialApp(debugShowCheckedModeBanner: false, 
     home: Login(),
